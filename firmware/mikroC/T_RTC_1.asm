725140c55e75eea56ddec1f2f16ec4a82ff3d9c3
Toni-Aguilar <51679829+Toni-Aguilar@users.noreply.github.com>
2019-06-11 08:29:56 -0600

    RTC program
    RTC I2C

---
+++ firmware/mikroC/T_RTC_1.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;T_RTC_1.c,1 :: 		void main() {
+;T_RTC_1.c,3 :: 		}
+L_end_main:
+	GOTO        $+0
+; end of _main
---
+++ firmware/mikroC/T_RTC_1.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/UTEQ/Documents/GitHub/xihmai/firmware/mikroC/T_RTC_1.c"
+void main() {
+
+}
