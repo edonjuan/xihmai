4936958f0017fc07309850888241bcc7bab7d42b
Eduardo Donjuan <mario.donjuan@uteq.edu.mx>
2019-06-11 09:27:23 -0500

    Temperature & Humidity program
    Control TH02 I2C

---
+++ firmware/mikroC/e_TYH_1.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;e_TYH_1.c,1 :: 		void main() {
+;e_TYH_1.c,3 :: 		}
+L_end_main:
+	GOTO        $+0
+; end of _main
---
+++ firmware/mikroC/e_TYH_1.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/lalod/Downloads/Github/xihmai/firmware/mikroC/e_TYH_1.c"
+void main() {
+
+}
